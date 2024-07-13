2726f3ab714e22a397f1febe110d914f13919783
Shuflduf <shuflduf@gmail.com>
2024-07-12 20:45:41 -0600

    in water offset

--- player.gd
+++ player.gd
@@ -3,16 +3,24 @@ extends CharacterBody3D
 
 @onready var camera: Camera3D = $Camera3D
 @onready var sprite: Sprite3D = $Sprite3D
-@onready var area: Area3D = $Area3D
+#@onready var area: Area3D = $Area3D
 
 @export var speed = 5.0
 @export var jump_height = 8.5
 @export var lookahead = 4
 @export var total_jumps = 2
+@export var in_water_offset = Vector3(0, 0, 10)
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var camera_x_offset: float
 var jumps = total_jumps
+var weight_mult = 1
+var in_water = false
+var default_cam_pos
+
+func _ready() -> void:
+	default_cam_pos = camera.position
+
 
 func _physics_process(delta: float) -> void:
 	camera.position.x = position.x + camera_x_offset
@@ -20,8 +28,12 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		jumps = total_jumps
 	else:
-		velocity.y -= gravity * delta
-
+		velocity.y -= gravity * delta * weight_mult
+	
+	if Input.is_action_pressed("down"):
+		weight_mult = 3
+	else:
+		weight_mult = 1
 
 	if Input.is_action_just_pressed("jump") and jumps > 0:
 		velocity.y = jump_height
@@ -46,12 +58,17 @@ func change_dir_left(dir):
 
 
 
-
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area is Water:
-		gravity = -ProjectSettings.get_setting("physics/3d/default_gravity")
+		in_water = true
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(camera, "position", position + in_water_offset + default_cam_pos, 0.5)
+		gravity = -ProjectSettings.get_setting("physics/3d/default_gravity") * area.gravity_mult
 
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	if area is Water:
-		gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+		in_water = false
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(camera, "position", position + default_cam_pos, 0.5)
+		gravity = ProjectSettings.get_setting("physics/3d/default_gravity") 
--- scripts/water.gd
+++ scripts/water.gd
@@ -1,4 +1,4 @@
 class_name Water
 extends Area3D
 
-#@export var
+@export var gravity_mult = 0.7
