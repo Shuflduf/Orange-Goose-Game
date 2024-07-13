a235dc38175e9cded981e9128ee47defea31c216
Shuflduf <shuflduf@gmail.com>
2024-07-12 20:12:58 -0600

    camera looksahead

--- player.gd
+++ player.gd
@@ -3,27 +3,36 @@ extends CharacterBody3D
 @onready var camera: Camera3D = $Camera3D
 @onready var sprite: Sprite3D = $Sprite3D
 
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 8.5
+@export var speed = 5.0
+@export var jump_height = 8.5
+@export var lookahead = 4
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var camera_x_offset: float
 
 func _physics_process(delta: float) -> void:
-	camera.position.x = position.x
+	camera.position.x = position.x + camera_x_offset
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_height
 
 	var input_dir := Input.get_axis("left", "right")
 	var direction := (transform.basis * Vector3(input_dir, 0, 0)).normalized()
 	if direction:
-		sprite.flip_h = true if input_dir < 0 else false
-		velocity.x = direction.x * SPEED
+		change_dir_left(input_dir < 0)
+		velocity.x = direction.x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+func change_dir_left(dir):
+	if sprite.flip_h != dir:
+		var mult = -1 if dir else 1
+		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property(self, "camera_x_offset", lookahead * mult, 0.5)
+	sprite.flip_h = dir
