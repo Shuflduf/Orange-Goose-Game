d286ae0ed3174745225ee9f62ef003f5b3a84e93
Shuflduf <shuflduf@gmail.com>
2024-07-12 20:05:45 -0600

    camera z simply follows player

--- player.gd
+++ player.gd
@@ -1,26 +1,27 @@
 extends CharacterBody3D
 
+@onready var camera: Camera3D = $Camera3D
+@onready var sprite: Sprite3D = $Sprite3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 8.5
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 8.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	camera.position.x = position.x
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_axis("left", "right")
 	var direction := (transform.basis * Vector3(input_dir, 0, 0)).normalized()
 	if direction:
+		sprite.flip_h = true if input_dir < 0 else false
 		velocity.x = direction.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
