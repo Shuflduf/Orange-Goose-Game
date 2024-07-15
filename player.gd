450524a4e3a572cc7bde7f6306812bd83cf199dc
Shuflduf <shuflduf@gmail.com>
2024-07-14 21:27:48 -0600

    better camera system, fixed bugs

--- player.gd
+++ player.gd
@@ -2,29 +2,33 @@ class_name Player
 extends CharacterBody3D
 
 @onready var camera: Camera3D = $Camera3D
-@onready var sprite: Sprite3D = $Sprite3D
-#@onready var area: Area3D = $Area3D
+@onready var sprites: Node3D = $Sprites
 
 @export var speed = 5.0
 @export var jump_height = 8.5
-@export var lookahead = 4
 @export var total_jumps = 2
-@export var in_water_offset = Vector3(0, 0, 10)
+
+@export_category("Camera Offsets")
+@export var default_water_offset = Vector2(0, 10)
+@export var default_ahead_offset = 4
+
+var ahead_offset: float
+var water_offset: Vector2 # Y and Z
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-var camera_x_offset: float
-var camera_water_offset
 var jumps = total_jumps
 var weight_mult = 1
 var in_water = false
-var default_cam_pos
+var following_player_y = false
+var default_cam_pos: Vector3
 
 func _ready() -> void:
 	default_cam_pos = camera.position
 
 
 func _physics_process(delta: float) -> void:
-	camera.position.x = position.x + camera_x_offset
+	camera.position.x = default_cam_pos.x + position.x + ahead_offset
+	camera.position.z = default_cam_pos.z + water_offset.y
 	#camera.position 
 	
 	if is_on_floor():
@@ -51,21 +55,19 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func change_dir_left(dir):
-	if sprite.flip_h != dir:
+func change_dir_left(dir: bool):
+	if sprites.scale.x < 0 == dir:
+		flip_sprites(dir)
 		var mult = -1 if dir else 1
 		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-		tween.tween_property(self, "camera_x_offset", lookahead * mult, 0.5)
-	sprite.flip_h = dir
-
+		tween.tween_property(self, "ahead_offset", default_ahead_offset * mult, 0.5)
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area is Water:
 		in_water = true
 		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-		tween.tween_property(camera, "position", \
-		position + in_water_offset + default_cam_pos + Vector3(camera_x_offset, 0, 0), 0.5)
+		tween.tween_property(self, "water_offset", default_water_offset, 0.5)
 		gravity = -ProjectSettings.get_setting("physics/3d/default_gravity") * area.gravity_mult
 
 
@@ -73,6 +75,10 @@ func _on_area_3d_area_exited(area: Area3D) -> void:
 	if area is Water:
 		in_water = false
 		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-		tween.tween_property(camera, "position", \
-		position + default_cam_pos + Vector3(camera_x_offset, 0, 0), 0.5)
+		tween.tween_property(self, "water_offset", Vector2.ZERO, 0.5)
 		gravity = ProjectSettings.get_setting("physics/3d/default_gravity") 
+
+func flip_sprites(dir: bool):
+	var offset = 1 if dir else -1
+	sprites.scale.x = 8 * offset
+
