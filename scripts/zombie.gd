52934825ca8c7e4f761549c54ac67b1eb107fb64
Shuflduf <shuflduf@gmail.com>
2024-07-18 21:53:07 -0600

    attack cooldown

--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprites: Node3D = $Sprites
+@onready var attack_cooldown: Timer = $AttackCooldown
 
 @export var speed = 5.0
 
@@ -9,6 +10,8 @@ var target: Player
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var gravity_mult = 1.5
+
+var attacking = false
 
 func _on_player_detect_body_entered(body: Node3D) -> void:
 	if body is Player:
@@ -24,27 +27,39 @@ func _on_player_detect_body_exited(body: Node3D) -> void:
 	
 	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta * gravity_mult
 	
-	velocity = Vector3.ZERO
-	var move_dir = Vector3.ZERO
-	if target != null:
-		move_dir = global_position.direction_to(target.global_position)
+	velocity.x = 0
+	
+	if attacking:
+		return
+	
+	if target != null: 
+		if global_position.distance_to(target.global_position) < 4:
+			print("AH")
+			animation.play("RESET", 0)
+			animation.queue("attack")
+			animation.queue("idle")
+			attacking = true
+			attack_cooldown.start()
+			return
+			
+		var move_dir = global_position.direction_to(target.global_position)
 		move_dir *= Vector3.RIGHT
 		move_dir = move_dir.normalized()
+		sprites.scale.x = -8 * move_dir.x
 		velocity.x = move_dir.x * speed
-		var dir = 1 if move_dir.x < 0 else -1
-		sprites.scale.x = 8 * dir
-		
-	
-	
 	
 	move_and_slide()
 
 
+#func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	#if anim_name == "attack":
+		#attacking = false
+		#animation.play("idle")
 
 
-
-
+func _on_attack_cooldown_timeout() -> void:
+	attacking = false
+	#animation.pla
