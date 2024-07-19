e1c85bd82dba01381a55b13635bee52d29f3df9f
Shuflduf <shuflduf@gmail.com>
2024-07-18 20:12:05 -0600

    basic backround

--- scripts/backround.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-@onready var texture: TextureRect = %TextureRect
-
-func set_texture_offset(new_offset):
-	texture.material.set_shader_parameter("offset", new_offset)
---
+++ scripts/world.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+@onready var backround: Sprite3D = $Backround
+@onready var player: Player = $Player
+
+@export var backround_offset = 130
+@export_range(0, 1, 0.05) var parallax_effect = 0.6
+
+var camera: Camera3D:
+	get:
+		return player.find_child("Camera3D")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	backround.position.x = camera.position.x
+	backround.position.y = camera.position.y
