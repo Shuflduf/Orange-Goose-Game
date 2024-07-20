73617e72beb3c48f46e30d49f8a6afc24cabb5e3
Shuflduf <shuflduf@gmail.com>
2024-07-19 21:53:38 -0600

    fixed camera height not changing when respawning

--- scripts/respawn.gd
+++ scripts/respawn.gd
@@ -1,8 +1,10 @@
 class_name RespawnPoint
 extends Area3D
 
+@export var camera_height: float
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
 		EnemyManager.save_all_enemies()
 		body.respawn_point = global_position
+		body.world_offset = camera_height
