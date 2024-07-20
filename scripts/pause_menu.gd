f4aa0c4ab6dccf01c36000deffa81b7aa51994f6
Shuflduf <shuflduf@gmail.com>
2024-07-20 14:23:47 -0600

    scene manager

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -7,13 +7,11 @@ func _ready() -> void:
 	hide()
 
 func _unhandled_key_input(event: InputEvent) -> void:
-
 	if event.is_action_pressed("pause"):
 		ui.pause_state.emit(!visible)
 		visible = !visible
 	
-
-
+	
 func _on_continue_pressed() -> void:
 	visible = !visible
 	ui.pause_state.emit(visible)
