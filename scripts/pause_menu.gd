7615cd164637f372db2d2d8ac4230e3fa6e101a7
Shuflduf <shuflduf@gmail.com>
2024-07-21 14:10:41 -0600

    view controls

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,12 +2,19 @@
 extends Control
 
 @onready var ui: Control = $".."
+@onready var buttons: VBoxContainer = $CenterContainer/Buttons
+@onready var controls: VBoxContainer = $CenterContainer/Controls
 
 func _ready() -> void:
 	hide()
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
+		if controls.visible:
+			controls.hide()
+			buttons.show()
+			return
+		
 		if ui.win_screen.visible:
 			return
 		ui.pause_state.emit(!visible)
@@ -25,3 +32,13 @@ func _on_main_menu_pressed() -> void:
 
 func _on_restart_pressed() -> void:
 	ui.reload.emit()
+
+
+func _on_controls_pressed() -> void:
+	buttons.hide()
+	controls.show()
+
+
+func _on_done_pressed() -> void:
+	buttons.show()
+	controls.hide()
